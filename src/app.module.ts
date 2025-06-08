370715a30ff2aeb07dadae74086fea54169f50f6
Gustavo361 <gustavofaustino361@gmail.com>
2025-06-08 19:10:58 -0300

    modified app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { RecipeController } from './controllers/recipe.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [RecipeController],
+  providers: [],
 })
 export class AppModule {}
